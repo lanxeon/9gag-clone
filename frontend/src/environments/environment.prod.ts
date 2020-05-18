e7df88590786462fcaecb7591a9225aba9b0b34c
lanxeon <lanxeon@users.noreply.github.com>
2020-05-18 23:52:37 +0530

    Seems like it was a one-off bug for that specific image

--- frontend/src/environments/environment.prod.ts
+++ frontend/src/environments/environment.prod.ts
@@ -1,4 +1,5 @@
 export const environment = {
   production: true,
-  apiUrl: "http://9gag-env.eba-htkhrf5w.us-east-2.elasticbeanstalk.com"
+  // apiUrl: "http://9gag-env.eba-htkhrf5w.us-east-2.elasticbeanstalk.com"
+  apiUrl: "http://9gag-env.eba-wfmpg5pv.ap-south-1.elasticbeanstalk.com"
 };
